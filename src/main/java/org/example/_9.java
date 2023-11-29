0c42df25086136ce5ade9aafb05a13a578b9128b
Nathan Vasconcelos Garrett <79205008+nathangarret@users.noreply.github.com>
2023-11-28 23:09:16 -0300

    _9.java

--- src/main/java/org/example/_9.java
+++ src/main/java/org/example/_9.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class _9 {
     public static void main(String[] args) {
         // 9) Crie um loop infinito que solicita ao usuário um número. Pare o loop quando o número 0
