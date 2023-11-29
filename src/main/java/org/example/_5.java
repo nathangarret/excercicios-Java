995cc1c42b0d1aa5dc6b5156399e26e8aa06f192
Nathan Vasconcelos Garrett <79205008+nathangarret@users.noreply.github.com>
2023-11-28 22:35:02 -0300

    _5.java

--- src/main/java/org/example/_5.java
+++ src/main/java/org/example/_5.java
@@ -3,6 +3,11 @@ package org.example;
 public class _5 {
     public static void main(String[] args) {
         // 5)Imprima os números de 1 a 10, mas pare o loop quando encontrar um número maior que 7.
-
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+            if (i > 7) {
+                break; // Para o loop quando encontrar um número maior que 7
+            }
+        }
     }
 }
