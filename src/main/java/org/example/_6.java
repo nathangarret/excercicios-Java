998216201cbf4cad7b8da532c24a8fa9820e3580
Nathan Vasconcelos Garrett <79205008+nathangarret@users.noreply.github.com>
2023-11-28 22:35:34 -0300

    _6.java

--- src/main/java/org/example/_6.java
+++ src/main/java/org/example/_6.java
@@ -3,5 +3,11 @@ package org.example;
 public class _6 {
     public static void main(String[] args) {
         // 6)Imprima os números de 1 a 10, pulando a impressão do número 5, usando continue.
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                continue; // Pula a impressão do número 5
+            }
+            System.out.println(i);
+        }
     }
 }
