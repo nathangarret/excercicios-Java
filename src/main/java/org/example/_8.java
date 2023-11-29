9d547c61e1a7ca2b65d45c8c7742e1902aa76d85
Nathan Vasconcelos Garrett <79205008+nathangarret@users.noreply.github.com>
2023-11-28 22:37:17 -0300

    _8.java

--- src/main/java/org/example/_8.java
+++ src/main/java/org/example/_8.java
@@ -4,5 +4,14 @@ public class _8 {
     public static void main(String[] args) {
         // 8)Imprima os números de 1 a 10, pulando a impressão do número 5 e interrompendo o loop quando
         // encontrar o número 8.
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                continue; // Pula a impressão do número 5
+            }
+            System.out.println(i);
+            if (i == 8) {
+                break; // Interrompe o loop quando encontrar o número 8
+            }
+        }
     }
 }
